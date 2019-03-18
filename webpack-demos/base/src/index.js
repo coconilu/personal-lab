e9866b6e2dcd4e2b308b47122f5f32fd75e7be91
alpha <827130441@qq.com>
2019-03-18 09:27:00 +0800

    feat: HtmlWebpackPlugin演示。

---
+++ webpack-demos/base/src/index.js
@@ -0,0 +1,5 @@
+import css from "./index.css"
+
+document.addEventListener("DOMContentLoaded", e => {
+  console.log("hello world")
+})
---
+++ webpack-demos/base/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const extractPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  watch: true,
+  output: {
+    path: __dirname + "/output",
+    publicPath: "/output/",
+    filename: "bundle.js",
+    chunkFilename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+        query: {
+          presets: [
+            [
+              "@babel/env",
+              {
+                targets: {
+                  browsers: "last 2 chrome versions"
+                }
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/tpl.html"),
+      filename: "index.html",
+      hash: true,
+      inject: true,
+      compile: true,
+      favicon: false,
+      minify: false,
+      cache: true,
+      showErrors: true,
+      chunks: "all",
+      excludeChunks: [],
+      title: "Webpack App",
+      xhtml: false
+    })
+  ],
+  resolve: {
+    extensions: [".json", ".js", ".jsx"]
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.join("/dist/"),
+    inline: true,
+    host: "0.0.0.0",
+    port: 8088
+  }
+};
