273bf4620b7300e7181e9aa599a41daf47ecec71
bayeswang <bayeswang@tencent.com>
2019-03-22 14:06:00 +0800

    feat: 添加json-loader演示，webpack3及以上都默认支持json，不需要json-loader。

--- webpack-demos/base/src/index.js
+++ webpack-demos/base/src/index.js
@@ -3,6 +3,7 @@ import hello from "./hello.txt";
 import val from "./val";
 import piece from "./assert/piece.jpg";
 import sea from "./assert/sea.png";
+import demoJson from "./demo.json";
 
 document.addEventListener("DOMContentLoaded", e => {
   console.log("TCL: hello", hello);
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", e => {
   console.log("TCL: sea", sea);
   insertImg(piece);
   insertImg(sea);
+  console.log("TCL: demoJson", demoJson);
 });
 
 function insertImg(src) {
