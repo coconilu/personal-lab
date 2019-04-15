e172cf78a5cba212f069e44a6e55610a0df297d7
alpha <827130441@qq.com>
2019-04-15 09:00:59 +0800

    feat: 添加hook特性。

--- react-demos/react-webpack/src/App.js
+++ react-demos/react-webpack/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import MyButton from "./Button";
 
 class App extends Component {
   render() {
-    return <h1>Hello {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello {this.props.name}</h1>
+        <MyButton>click me!</MyButton>
+      </div>
+    );
   }
 }
 
